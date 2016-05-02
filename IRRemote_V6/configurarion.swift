63af55eb0025c2f103c7c3b6ae16a06e1a4d30b2
Yuchao99 <ycchensoftware@gmail.com>
2016-05-02 16:10:39 -0400

    add configuration files to static setting

---
+++ IRRemote_V6/configurarion.swift
@@ -0,0 +1,24 @@
+//
+//  configurarion.swift
+//  IRRemote_V6
+//
+//  Created by Yuchao Chen on 5/2/16.
+//  Copyright © 2016 Yuchao Chen. All rights reserved.
+//
+
+import Foundation
+
+class Configuration{
+    
+        let ruValue      = 4
+        let rdValue      = 5
+        let delValue     = 15
+        let maxDimValue  = 80
+        let minDimValue  = 1
+        let sensValue    = 100
+        let maxLuxValue  = 56
+        let minLuxValue  = 10
+        let lensValue    = 0
+        let alsValue     = 0
+
+}
